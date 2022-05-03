28879971f9242cae8603a446018bfba6f120e58f
matiasrocca <matiasrocca98@gmail.com>
2022-05-03 17:15:03 -0300

    cambios en el index

--- config/index.js
+++ config/index.js
@@ -1,9 +1,19 @@
-const dbEngine = process.env.DB_ENVIRONMENT || "development";
-const config = require("../config/config.json")[dbEngine];
+require("dotenv").config()
+const { options, database } = require("../config/config.json");
 const Sequelize = require("sequelize");
+const { DATABASE_URL } = process.env
+
+const db = new Sequelize(`${DATABASE_URL}`, {
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+  });
 
 
-const db = new Sequelize(config);
+// const db = new Sequelize(database, null, null, options);
 
 
 module.exports = db
