f8b6699e309a5ddbda6488a0b58c26136b0f6dea
matiasrocca <matiasrocca98@gmail.com>
2022-05-02 21:37:25 -0300

    added heroku config info

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 
-const { options, database } = require("../config/config.json");
+const config = require("../config/config.json");
 const Sequelize = require("sequelize");
 
 
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(config);
 
 
 module.exports = db
