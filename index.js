615190ad9e36fefcb35b9a6a5c2c2c1145411f4a
matiasrocca <matiasrocca98@gmail.com>
2022-05-05 17:30:55 -0300

    vuelta al cors basico

--- index.js
+++ index.js
@@ -68,11 +68,7 @@ passport.deserializeUser(function(id, done) {
       .catch(done)
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors())
 
 app.use("/api", routes);
 
